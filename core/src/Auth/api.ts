ee9944e02c7e8795df85c3c8b1b2992ac414cfe3
plecsi <57689534+plecsi@users.noreply.github.com>
2025-08-11 01:48:57 +0200

    Delete core/src/Auth directory

--- core/src/Auth/api.ts
+++
@@ -1,5 +0,0 @@
-import { generateAuthApi } from '../store/api';
-
-const authApi = generateAuthApi();
-
-export default authApi;
--- core/src/Auth/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './api';
-export * from './hook';
-export * from './saga';
-export * from './slice';
-export * from './selectors';
-export * from './types';
-
-export * from './components/Login';
